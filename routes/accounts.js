100c20fea15ab50edea11fbbf00c63c2966d87ee
ohmono <basura1300@gmail.com>
2021-05-05 13:33:53 -0500

    INPROVED json structure

--- routes/accounts.js
+++ routes/accounts.js
@@ -2,10 +2,9 @@
 const accountRoutes = (app, fs) => {
 
     // variables
-    const dataPath = './db/public-accounts.json';
+    dataPath = './db/err/err.json'
 
-
-    // helper methods
+    // helper methods //segregate
     const readFile = (callback, returnJson = false, filePath = dataPath, encoding = 'utf8') => {
         fs.readFile(filePath, encoding, (err, data) => {
             if (err) {
@@ -27,89 +26,96 @@ const accountRoutes = (app, fs) => {
         });
     };
 
-    // READ ALL
-    app.get('/accounts', (req, res) => {
-        fs.readFile(dataPath, 'utf8', (err, data) => {
-            if (err) {
-                throw err;
-            }
 
-            res.send(JSON.parse(data));
+    /*
+        // READ ALL
+        app.get('/accounts', (req, res) => {
+            fs.readFile(dataPath, 'utf8', (err, data) => {
+                if (err) {
+                    throw err;
+                }
+    
+                res.send(JSON.parse(data));
+            });
         });
-    });
-
-    // READ ONE
-    app.get('/account/:id', (req, res) => {
-        fs.readFile(dataPath, 'utf8', (err, data) => {
-            if (err) {
-                throw err;
-            }
-            const accountId = req.params["id"]
-            res.send(JSON.parse(data)[accountId.toString()])
-        });
-    });
+    
+        // READ ONE
+        app.get('/account/:id', (req, res) => {
+            fs.readFile(dataPath, 'utf8', (err, data) => {
+                if (err) {
+                    throw err;
+                }
+                const accountId = req.params["id"]
+                res.send(JSON.parse(data)[accountId.toString()])
+            });
+        });*/
 
     // CREATE
     app.post('/accounts', (req, res) => {
-
+        if (req.body['_accid'] == '_sav')
+            dataPath = './db/saving-accounts.json';
+        else if (req.body['_accid'] == '_cur')
+            dataPath = './db/current-accounts.json';
+        else
+            res.status(500).send("algo salio mal :''(");
         readFile(data => {
-            const newaccountId = new Date(Date.now());
+            // const newaccountId = new Date(Date.now());
             const prueb = JSON.stringify(data)
 
-            //  console.log(JSON.parse(JSON.stringify(req.body)))
-            if (req.body['rent'] != 0)
-                accType = '_sav'
-            else
-                accType = '_cur'
             n = 1;
-            while (JSON.parse(prueb)[accType + n.toString()] != null) {
+            while (JSON.parse(prueb)[req.body['_accid'] + n.toString()] != null) {
                 n++;
             }
-            req.body["creationDate"] = newaccountId.toUTCString()
+            // req.body["creationDate"] = newaccountId.toUTCString()
+            if (req.body['_accid'] == '_sav')
+                req.body['rent'] = 0.05
+
             req.body["transactions"] = []
-            req.body["status"] = "false"
+            req.body["status"] = false
             req.body["money"] = 0
+            accid = req.body['_accid'] + n.toString()
+            delete req.body._accid
+            data[accid] = req.body;
 
-            data[accType + n.toString()] = req.body;
 
             writeFile(JSON.stringify(data, null, 2), () => {
-                res.status(200).json({ "_accid": accType + n.toString() });
-            });
+                res.status(200).json({ "_accid": accid });
+            }, dataPath);
         },
             true);
     });
 
-
-    // UPDATE
-    app.put('/account/:id', (req, res) => {
-
-        readFile(data => {
-
-            const accountId = req.params["id"];
-
-            data[accountId] = req.body;
-
-            writeFile(JSON.stringify(data, null, 2), () => {
-                res.status(200).send(`accounts id:${accountId} updated`);
-            });
-        },
-            true);
-    });
-
-
-    // DELETE
-    app.delete('/account/:id', (req, res) => {
-
-        readFile(data => {
-            const accountId = req.params["id"];
-            delete data[accountId];
-
-            writeFile(JSON.stringify(data, null, 2), () => {
-                res.status(200).send(`accounts id:${accountId} removed`);
-            });
-        },
-            true);
-    });
+    /*
+        // UPDATE
+        app.put('/account/:id', (req, res) => {
+    
+            readFile(data => {
+    
+                const accountId = req.params["id"];
+    
+                data[accountId] = req.body;
+    
+                writeFile(JSON.stringify(data, null, 2), () => {
+                    res.status(200).send(`accounts id:${accountId} updated`);
+                });
+            },
+                true);
+        });
+    
+    
+        // DELETE
+        app.delete('/account/:id', (req, res) => {
+    
+            readFile(data => {
+                const accountId = req.params["id"];
+                delete data[accountId];
+    
+                writeFile(JSON.stringify(data, null, 2), () => {
+                    res.status(200).send(`accounts id:${accountId} removed`);
+                });
+            },
+                true);
+        });*/
 };
 
 module.exports = accountRoutes;
