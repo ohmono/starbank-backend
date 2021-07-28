6e3cb233c6f061f6bb704ac82f8105cc1e7990af
ohmono <basura1300@gmail.com>
2021-07-27 21:35:40 -0500

    improve the separation of json db

--- controllers/account.controller.js
+++ controllers/account.controller.js
@@ -7,22 +7,29 @@ const crypto = require('crypto');
 
 // create one account
 function create_account(req, res) {
-    var accounts = getAccountData()
+    var account_type = req.body.account_type
+    var accounts = getAccountData(account_type)
     var newId = crypto.randomBytes(8).toString("hex");
-    var accountType = req.body.account_type
     delete req.body.account_type
-    accounts[`${accountType}_${newId}`] = Object.assign({}, req.body, {
+    accounts[newId] = Object.assign({}, req.body, {
         "transactions": [],
         "isActive": false,
         "balance": 0
     });
-    saveAccountData(accounts);
+    saveAccountData(accounts, account_type);
     res.send({ success: true, msg: 'account created successfully' })
 }
 
-// get all accounts
-function view_all_accounts(req, res) {
-    var accounts = getAccountData()
+// get all saving accounts
+function view_all_saving_accounts(req, res) {
+    var accounts = getAccountData('saving')
+
+    res.send(accounts)
+}
+
+// get all saving accounts
+function view_all_current_accounts(req, res) {
+    var accounts = getAccountData('current')
 
     res.send(accounts)
 }
@@ -30,36 +37,67 @@ function view_all_accounts(req, res) {
 // get one account by id
 function view_one_account(req, res) {
     var id = req.params.id
-    var account = getAccountData()[id]
-
+    var accounts = Object.assign({}, getAccountData('current'), getAccountData('saving'))
+    var account = accounts[id]
     res.send(account)
 }
 
 // update one account by id
 function update_account(req, res) {
     var id = req.params.id;
-    var accounts = getAccountData();
 
-    accounts[id] = req.body;
-    saveAccountData(accounts);
-    res.send(`accounts with id ${id} has been updated`);
+    function search_update(id, account_type) {
+        var accounts = getAccountData(account_type);
+        for (const key of Object.keys(req.body)) {
+            accounts[id][key] = (req.body[key])
+        }
+        saveAccountData(accounts, account_type);
+        res.send(`accounts with id ${id} has been updated`);
+    }
+
+    if (getAccountData('current')[id]) {
+        var account_type = 'current'
+        return search_update(id, account_type);
+    }
+    else if (getAccountData('saving')[id]) {
+        var account_type = 'saving'
+        return search_update(id, account_type);
+    }
+    else {
+        res.send(`accounts with id ${id} not exist`);
+    }
 }
 
 // delete one account by id
 function delete_account(req, res) {
     var id = req.params.id;
-    var accounts = getAccountData();
 
-    delete accounts[id];
-    saveAccountData(accounts);
-    res.send(`accounts with id ${id} has been deleted`)
+    function search_delete(id, account_type) {
+        var accounts = getAccountData(account_type);
+        delete accounts[id]
+        saveAccountData(accounts, account_type);
+        res.send(`accounts with id ${id} has been deleted`);
+    }
+
+    if (getAccountData('current')[id]) {
+        var account_type = 'current'
+        return search_delete(id, account_type);
+    }
+    else if (getAccountData('saving')[id]) {
+        var account_type = 'saving'
+        return search_delete(id, account_type);
+    }
+    else {
+        res.send(`accounts with id ${id} not exist`);
+    }
 
 }
 
 // export route controllers
 module.exports = {
     create_account,
-    view_all_accounts,
+    view_all_saving_accounts,
+    view_all_current_accounts,
     view_one_account,
     update_account,
     delete_account
